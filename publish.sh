b520a73cbf7d75d65197e8c9b644669a276ef49a
Nathan Jones <nathan@ncjones.com>
2015-04-04 18:09:37 +1300

    add index.html to update site

--- publish.sh
+++ publish.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 base_dir=$(cd $(dirname $0) && pwd)
-site_dir=$base_dir/site
+site_src_dir=$base_dir/site/src
+site_build_dir=$base_dir/site/target
 p2_repo_dir=$base_dir/editorconfig-eclipse-p2/target/repository
-rm -rf $site_dir
-mkdir $site_dir
+rm -rf $site_build_dir
+mkdir $site_build_dir
 if [ ! -d $p2_repo_dir ]; then
   echo ERROR: $p2_repo_dir does not exist. Run '`mvn install`'.
   exit 1
 fi
-cp -r $p2_repo_dir $site_dir
-cd $site_dir
+cp -r $p2_repo_dir $site_build_dir
+cp -r $site_src_dir/* $site_build_dir
+cd $site_build_dir
 git init
 git add .
 git commit -m 'add p2 repo'

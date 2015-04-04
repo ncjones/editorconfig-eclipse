a150e0779b7b86747a46f65a1ba234e6ff096eb0
Nathan Jones <nathan@ncjones.com>
2015-04-04 21:31:47 +1300

    fix plugin version ref in feature.xml

--- update-versions.sh
+++ update-versions.sh
@@ -3,4 +3,6 @@ if [ -z "$1" ]; then
   echo "usage: $0 <new version>"
   exit 1
 fi
-mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$1
+new_version=$1
+mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$new_version-SNAPSHOT
+find -name 'feature.xml' | xargs sed -i -r "s/version=\"[0-9]+\.[0-9]+\.[0-9]+\"/version=\"$new_version\"/"

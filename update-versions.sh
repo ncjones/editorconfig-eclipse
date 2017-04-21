99577a9770fc3b71365ce97989782c235ee1ed20
Nathan Jones <nathan@ncjones.com>
2017-04-21 22:08:33 +1200

    Fix update versions script
    Functional test project version was overlooked. Also adds portability for OSX/BSD.

--- update-versions.sh
+++ update-versions.sh
@@ -3,12 +3,31 @@
 set -e
 
 if [ -z "$1" ]; then
-  echo "usage: $0 <new version>"
+  echo "usage: $(basename "$0") <new version>"
   exit 1
 fi
 
+cd "$(dirname "$0")"
+
 new_version=$1
-mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion="$new_version-SNAPSHOT"
-find -name 'feature.xml' -exec sed -i -r "s/version=\"[0-9]+\.[0-9]+\.[0-9]+\"/version=\"$new_version\"/" '{}' ';'
+version_pattern="[0-9]+\.[0-9]+\.[0-9]+"
+
+mvn \
+  -Dtycho.mode=maven \
+  -DnewVersion="$new_version-SNAPSHOT" \
+  org.eclipse.tycho:tycho-versions-plugin:set-version
+
+find . \
+  -name 'feature.xml' \
+  -exec sed -i.bak -E "s/version=\"${version_pattern}\"/version=\"${new_version}\"/" '{}' ';'
+
+find ./editorconfig-eclipse-functional-test \
+  -name pom.xml \
+  -exec sed -i.bak -E "s/${version_pattern}-SNAPSHOT/${new_version}-SNAPSHOT/" '{}' ';'
+
+find ./editorconfig-eclipse-functional-test \
+  -name MANIFEST.MF \
+  -exec sed -i.bak -E "s/${version_pattern}.qualifier/${new_version}.qualifier/" '{}' ';'
+
 git add .
 git commit -m "Update version to $new_version"
